c6c45979d05d6712fbaa637ef7f86b138f00a2d3
Ugendarkumar J P <ugendarkumar_j@photoninfotech.net>
2018-11-30 19:11:09 +0530

    code format

--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,29 @@ const router = express.Router();
 
 
 
-router.get('/root',(req,res) => {
+router.get('/root', (req, res) => {
     return res.send(`Hello world`);
 });
 
-router.get('/createTable',(req,res) => {
-    controller.createTable(req,res);
+router.get('/createTable', (req, res) => {
+    controller.createTable(req, res);
 })
 
-router.get('/updatePrimaryKey',(req,res) => {
-    controller.updatePrimarykey(req,res);
+router.get('/updatePrimaryKey', (req, res) => {
+    controller.updatePrimarykey(req, res);
 })
 
-router.get('/saveInTable',(req,res) => {
-    controller.saveInTable(req,res);
+router.get('/saveInTable', (req, res) => {
+    controller.saveInTable(req, res);
 })
 
-router.get('/saveMultipleInTable',(req,res) => {
-    controller.saveMultipleInTable(req,res);
+router.get('/saveMultipleInTable', (req, res) => {
+    controller.saveMultipleInTable(req, res);
 })
 
 
-router.get('/selectData',(req,res) => {
-    controller.selectData(req,res);
+router.get('/selectData', (req, res) => {
+    controller.selectData(req, res);
 })
 
 
